b85b0662d82c38246e5763db200de16287346403
Kimberley Cook <kimberleycook@Kimberleys-MacBook-Pro.local>
2016-03-29 16:59:42 +0100

    custom HTML full width

--- app/Hooks/RelatedContentAcf.php
+++ app/Hooks/RelatedContentAcf.php
@@ -1,22 +1,9 @@
 <?php namespace AgreableLongformPlugin\Hooks;
 
-class SocialMediaAcf {
+class RelatedContentAcf {
 
   public function init() {
-    \add_filter('agreable_base_theme_social_media_acf', array($this, 'social_media_acf'), 10);
     \add_filter('agreable_base_theme_related_acf', array($this, 'related_acf'), 11);
-  }
-
-  public function social_media_acf($acf_definition) {
-    $acf_definition['location'][] = [
-      [
-        'param' => 'post_type',
-        'operator' => '==',
-        'value' => 'longform',
-      ]
-    ];
-
-    return $acf_definition;
   }
 
   public function related_acf($acf_definition) {
