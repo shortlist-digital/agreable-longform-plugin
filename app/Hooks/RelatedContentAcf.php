93e8a1427c3af40d0e33b4b3415b5cf32b166242
Kimberley Cook <kimberleycook@Kimberleys-MacBook-Pro.local>
2016-03-30 13:22:49 +0100

    related content hook

--- app/Hooks/RelatedContentAcf.php
+++ app/Hooks/RelatedContentAcf.php
@@ -3,7 +3,7 @@
 class RelatedContentAcf {
 
   public function init() {
-    \add_filter('agreable_base_theme_related_acf', array($this, 'related_acf'), 11);
+    \add_filter('agreable_base_theme_related_acf', array($this, 'related_acf'), 10);
   }
 
   public function related_acf($acf_definition) {
